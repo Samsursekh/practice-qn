d4b2636f741e4524387481885c644d918c43a584
Samsur Sekh <sekhsamsur44@gmail.com>
2023-03-03 23:28:45 +0530

    some error occur in the sort

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const container = document.getElementById("container");
 let dataArr = [];
+const cardsPerPage = 12;
+const pagination = document.getElementById("pagination");
+
 const fetchDataFunction = async() => {
   const res = await fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products`);   
   
   dataArr = await res.json();
   let data = dataArr.data;
-  displayDunction(data);
+  displayDunction(data, 1);
 }
 
-const displayDunction = (data) => {
+const displayDunction = (data,currentPage) => {
     // console.log(data)
     container.innerHTML = '';
-    data.forEach(element => {
+
+    const firstCardIndex = (currentPage - 1) * cardsPerPage;
+    const lastCardIndex = firstCardIndex + cardsPerPage;
+
+    const currentProducts = dataArr.data.slice(firstCardIndex, lastCardIndex)
+
+    currentProducts.forEach(element => {
          const cards = document.createElement("cards");
          cards.classList.add("cards");
          
@@ -36,20 +45,55 @@ const displayDunction = (data) => {
          cards.append(img,title,brand,category,price,heartIcon);
          container.append(cards);
     });
+
+    const pageCount = Math.ceil(dataArr.data.length / cardsPerPage);
+    const pageLinksHtml = Array.from({length : pageCount}, (_, i) => `
+     <a class="page-link ${i + 1  === currentPage ? "active" : ""} " data-page= "${i + 1}" > ${i + 1} </a>
+    `).join("");
+
+    pagination.innerHTML = pageLinksHtml;
+
+    const pageLinks = pagination.querySelectorAll(".page-link");
+    pageLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            const pageNumber = link.dataset.page;
+            displayDunction(dataArr.data, pageNumber);
+        })
+    })
 }
 
+
+
 const filterByCategory = () => {
-//    console.log("Clicked.....")
-   console.log(dataArr, "Data Array")
    const selected = document.getElementById("categoryFilter").value;
    console.log(selected);
    let filteredList = dataArr.data.filter((element) => {
     return element.category === selected;
    })
-   console.log(filteredList);
-
-   displayDunction(filteredList);
+//    console.log(filteredList);
+   displayDunction(filteredList,1);
 }
 
+const sortByPrice = () => {
+    const selected = document.getElementById("sortPrice").value;
+    // console.log(selected)
+    // console.log(dataArr.data);
+    if(selected === "asc"){
+        currentProducts.sort((a,b) => {
+            return b.price - a.price;
+        })
+        console.log(dataArr);
+        displayDunction(currentProducts, 1);
+    }
+    if(selected === "dsc"){
+        currentProducts.sort((a,b) => {
+            return a.price - b.price;
+        })
+        console.log(dataArr);
+        displayDunction(currentProducts, 1);
+    }
+}
+
+//Sort ta kaj ho6e na kal dekhbo ja hobe
 
 fetchDataFunction();
